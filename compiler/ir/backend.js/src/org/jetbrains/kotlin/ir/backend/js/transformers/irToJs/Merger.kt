95bbfcf663205c77a0d97e3e361f4f3b209ca3b1
DendyGrobovshik <idenis.gradoboev@yandex.com>
2026-06-05 16:18:13 +0300

    fix : stable order for decls

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/Merger.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/Merger.kt
@@ -242,8 +242,8 @@ class Merger(
 
         val exportStatements = declareAndCallJsExporter() + additionalExports + transitiveJsExport()
 
-        val importedJsModules = this.importedModulesMap.values.toList() + this.crossModuleReferences.importedModules
-        val importStatements = this.importStatements.values.toList() + this.importStatementsWithEffect.toList()
+        val importedJsModules = this.importedModulesMap.values.sortedBy { it.internalName.ident }.toList() + this.crossModuleReferences.importedModules
+        val importStatements = this.importStatements.entries.sortedBy { it.key }.map { it.value }.toList() + this.importStatementsWithEffect.toList()
 
         val program = JsProgram()
 
@@ -299,7 +299,7 @@ class Merger(
         }
 
         DFS.dfs(
-            classModelMap.keys,
+            classModelMap.keys.sortedBy { it.ident },
             { classModelMap[it]?.dependsOnClasses ?: emptyList() },
             declarationHandler
         )
