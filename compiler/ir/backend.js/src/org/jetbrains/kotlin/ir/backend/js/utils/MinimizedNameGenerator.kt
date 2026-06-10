bd93d9e4e0eb1345786c4e783c4122c343576c1f
efremov.andrey <efremov.andrey65@rwb.ru>
2026-06-10 13:49:12 +0300

    Move Long.toStableJsIdentifier to js part

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/MinimizedNameGenerator.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/MinimizedNameGenerator.kt
@@ -5,9 +5,34 @@
 
 package org.jetbrains.kotlin.ir.backend.js.utils
 
-import org.jetbrains.kotlin.backend.common.serialization.toStableJsIdentifier
+import org.jetbrains.kotlin.backend.common.serialization.cityHash64
 import java.io.File
 
+
+private const val BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+fun Long.toStableJsIdentifier(): String {
+    val hash = this and 0x7FFFFFFFFFFFFFFF
+
+    val char4 = BASE62[(hash % 62).toInt()]
+    val char3 = BASE62[((hash / 62) % 62).toInt()]
+    val char2 = BASE62[((hash / (62 * 62)) % 62).toInt()]
+    val char1 = BASE62[((hash / (62 * 62 * 62)) % 52).toInt()]
+
+    return "$char1$char2$char3$char4"
+}
+
+fun String.toStableJsIdentifier(used: MutableSet<String>): String {
+    val hashId = this.cityHash64().toStableJsIdentifier()
+    var name = hashId
+    var suffix = 1
+    while (name in used) {
+        name = "$hashId$suffix"
+        suffix++
+    }
+    used.add(name)
+    return name
+}
 
 class MinimizedNameGenerator {
     private var index = 0
