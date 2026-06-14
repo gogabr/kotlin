ad64526d66ed99bed1e94369719e1ad999e8561d
efremov.andrey <efremov.andrey65@rwb.ru>
2026-06-14 12:17:41 +0300

    Remove sorting in DFS in JsNameLinkingNamer

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsNameLinkingNamer.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsNameLinkingNamer.kt
@@ -159,7 +159,9 @@ class JsNameLinkingNamer(
             val nameCnt = hashMapOf<String, Int>()
 
             val allClasses = DFS.topologicalOrder(listOf(this)) { node ->
-                node.superTypes.sortedBy { it.safeAs<IrSimpleType>()?.classifier?.signature?.render(IdSignatureRenderer.LEGACY) ?: "" }.mapNotNull {
+                node.superTypes
+//                    .sortedBy { it.safeAs<IrSimpleType>()?.classifier?.signature?.render(IdSignatureRenderer.LEGACY) ?: "" }
+                    .mapNotNull {
                     it.safeAs<IrSimpleType>()?.classifier.safeAs<IrClassSymbol>()?.owner
                 }
             }
